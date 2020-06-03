7e8a6680f19a48d8fdf5d3d4958958411c950dd9
unknown <digitone@naver.com>
2020-06-03 15:43:17 +0900

    section2-7

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 function Login(props) {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h2 className="mv3">Create Account</h2>
+      <form className="flex flex-column">
+        <input 
+          type="text"
+          placeholder="name"
+          autoComplete="off"
+        />
+        <input 
+          type="email"
+          placeholder="email"
+          autoComplete="off"
+        />
+        <input 
+          type="password"
+          placeholder="password"
+          autoComplete="off"
+        />
+        <div className="flex mt3">
+          <button type="submit" className="button pointer">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
